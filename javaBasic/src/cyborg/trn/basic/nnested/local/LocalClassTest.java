bff4f265123ce0371c9e84c00e11599df993abe8
mario.alvarez <mario.alvarez@LSTKEN40398.softtek.com>
2015-10-05 19:06:30 -0500

    Local Nested Class

---
+++ javaBasic/src/cyborg/trn/basic/nnested/local/LocalClassTest.java
@@ -0,0 +1,12 @@
+package cyborg.trn.basic.nnested.local;
+
+public class LocalClassTest {
+
+	public static void main(String[] args) {
+		NormalClass normalClass = new NormalClass();
+		
+		normalClass.exampleLocal();
+
+	}
+
+}
